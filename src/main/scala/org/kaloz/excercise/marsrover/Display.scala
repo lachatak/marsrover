43adc13ae1884c4e64af592d984c1526bc7a9572
Krisztian Lachata <krisztian.lachata@gmail.com>
2014-04-14 22:12:55 +0100

    add persistence to Display

--- src/main/scala/org/kaloz/excercise/marsrover/Display.scala
+++ src/main/scala/org/kaloz/excercise/marsrover/Display.scala
@@ -4,41 +4,43 @@ import akka.actor._
 import akka.persistence.{SnapshotOffer, EventsourcedProcessor}
 import scala.concurrent.duration._
 
+case class DisplayState(state: Map[ActorRef, RoverPosition] = Map.empty) {
+  def update(event: PositionChangedEvent) = copy(state + (event.marsRover -> event.roverPosition))
+}
+
+case class PositionChangedEvent(marsRover: ActorRef, roverPosition: RoverPosition)
+
 class Display extends EventsourcedProcessor with ActorLogging {
 
   import Display._
-
-  case class PositionChangedEvent(marsRover: String, roverPosition: RoverPosition)
-
-  case class DisplayState(roverPositions: Map[String, RoverPosition])
 
   case object TakeSnapshot
 
   context.system.scheduler.schedule(2 second, 2 second, self, TakeSnapshot)(context.dispatcher)
 
-  var roverPositions = Map.empty[String, RoverPosition]
+  var roverPositions = DisplayState()
 
   def updateState(evt: PositionChangedEvent) = evt match {
-    case PositionChangedEvent(marsRover, roverPosition) =>
+    case event@PositionChangedEvent(marsRover, roverPosition) =>
       log.info(s"Register $roverPosition to ${marsRover}")
-      roverPositions += (marsRover -> roverPosition)
+      roverPositions = roverPositions.update(event)
   }
 
   val receiveRecover: Receive = {
     case evt: PositionChangedEvent => updateState(evt)
-    case SnapshotOffer(_, DisplayState(positions)) => roverPositions = positions
+    case SnapshotOffer(_, snapshot: DisplayState) => roverPositions = snapshot
   }
 
   def receiveCommand: Receive = {
     case RegisterPosition(marsRover, roverPosition) =>
-      persist(PositionChangedEvent(marsRover.path.name, roverPosition))(updateState)
+      persist(PositionChangedEvent(marsRover, roverPosition))(updateState)
     case ShowPositions =>
-      roverPositions.foreach {
-        case (marsRover, position) => log.info(s"Last known position of ${marsRover} is $position")
+      roverPositions.state.foreach {
+        case (marsRover, position) => log.info(s"Last known position of ${marsRover.path.name} is $position")
       }
     case TakeSnapshot =>
-      println("Taking snapshot...")
-      saveSnapshot(DisplayState(roverPositions))
+      log.info("Taking snapshot...")
+      saveSnapshot(roverPositions)
   }
 }
 
--- src/test/scala/org/kaloz/excercise/marsrover/DisplayTest.scala
+++ src/test/scala/org/kaloz/excercise/marsrover/DisplayTest.scala
@@ -20,17 +20,17 @@ with BeforeAndAfterAll {
       display ! RegisterPosition(rover1.ref, RoverPosition(1, 2, E))
       display ! RegisterPosition(rover2.ref, RoverPosition(2, 3, E))
 
-      assert(display.underlyingActor.roverPositions.size == 2)
+      assert(display.underlyingActor.roverPositions.state.size == 2)
 
-//      assert(display.underlyingActor.roverPositions(rover1.ref.path.name) == RoverPosition(1, 2, E))
-//      assert(display.underlyingActor.roverPositions(rover2.ref.path.name) == RoverPosition(2, 3, E))
+      assert(display.underlyingActor.roverPositions.state(rover1.ref) == RoverPosition(1, 2, E))
+      assert(display.underlyingActor.roverPositions.state(rover2.ref) == RoverPosition(2, 3, E))
     }
 
     "print the result" in new scope {
       display ! RegisterPosition(rover1.ref, RoverPosition(1, 2, E))
       display ! RegisterPosition(rover2.ref, RoverPosition(2, 3, E))
 
-      assert(display.underlyingActor.roverPositions.size == 2)
+      assert(display.underlyingActor.roverPositions.state.size == 2)
 
       EventFilter.info(start = "Last known position", occurrences = 2) intercept {
         display ! ShowPositions
